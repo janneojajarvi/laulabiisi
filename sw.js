e3ff9cf31802c38edc22dc0f173a4989eaac299c
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-29 10:12:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'laulabiisi-v1'; // Päivitä aina versio, kun muutat ASSETS-listaa
+const CACHE_NAME = 'laulabiisi-v2'; // Päivitä aina versio, kun muutat ASSETS-listaa
 
 const ASSETS = [
   './',
@@ -6,10 +6,54 @@ const ASSETS = [
   './style.css',
   './script.js',
   './manifest.json',
+  './favicon.ico',
   './kuva.png',
+  
+  // Peruskirjastot ja testit
+  './FinnishTunes.js',
+  './FinnishTunes2.js',
+  './suomitest3.js',
+  './norway1.js',
+  './swedish2.js',
+  
+  // Esävelmät-sarja
+  './esavelmat_hs1.js',
+  './esavelmat_kansantanssit.js',
+  './esavelmat_kjs.js',
+  './esavelmat_ls1.js',
+  './esavelmat_ls2.js',
+  './esavelmat_ls3.js',
+  './esavelmat_ls4.js',
+  './esavelmat_rs1.js',
+  './esavelmat_rs2.js',
+  
+  // Folkwiki ja muut kokoelmat
+  './folkwikiSet1.js',
+  './folkwikiSet2.js',
+  './folkwikiSet3.js',
+  './fsfolkdiktning01.js',
+  './fsfolkdiktning02.js',
+  './extrasetti5.js',
+  
+  // SessionSet-sarja (01-18)
   './sessionSet01.js',
-  // ... muut tiedostot ...
-  './extrasetti5.js'
+  './sessionSet02.js',
+  './sessionSet03.js',
+  './sessionSet04.js',
+  './sessionSet05.js',
+  './sessionSet06.js',
+  './sessionSet07.js',
+  './sessionSet08.js',
+  './sessionSet09.js',
+  './sessionSet10.js',
+  './sessionSet11.js',
+  './sessionSet12.js',
+  './sessionSet13.js',
+  './sessionSet14.js',
+  './sessionSet15.js',
+  './sessionSet16.js',
+  './sessionSet17.js',
+  './sessionSet18.js'
 ];
 
 // 1. Asennusvaihe
